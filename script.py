ab7dbbfeab8c63fecb42e93193e89ef53a90321a
antoine <153529541+antoineswg@users.noreply.github.com>
2025-06-24 15:56:12 +0200

    fonction déchiffrage faite

--- script.py
+++ script.py
@@ -26,8 +26,29 @@ def encrypt(input):
 
 
 def decrypt(input):
-    
-    return "Faut que j'écrive la fonction là"
+    mapping = {
+        '2': 'a', '22': 'b', '222': 'c',
+        '3': 'd', '33': 'e', '333': 'f',
+        '4': 'g', '44': 'h', '444': 'i',
+        '5': 'j', '55': 'k', '555': 'l',
+        '6': 'm', '66': 'n', '666': 'o',
+        '7': 'p', '77': 'q', '777': 'r', '7777': 's',
+        '8': 't', '88': 'u', '888': 'v',
+        '9': 'w', '99': 'x', '999': 'y', '9999': 'z',
+        '0': ' '
+    }
+    output = []
+    tokens = input.strip().split()
+    i = 0
+    while i < len(tokens):
+        token = tokens[i]
+        if token in mapping:
+            output.append(mapping[token])
+            i += 1
+        else:
+            output.append(token)
+            i += 1
+    return "".join(output)
 
 
 mode = input("Déchiffrer (D) ou Chiffrer (C) ? ").lower()
